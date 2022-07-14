3a7ab459942097a110a8456778e04a7a8a2199af
@rharison <rharison.abreu@gmail.com>
2022-07-14 09:43:01 -0300

    feat: move index to dir src

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 import puppeteer from 'puppeteer';
 
-async function collectData(page) {
+async function getRelateds(page) {
   console.log('Collecting relateds...');
 
   const selectorAllLinksWithDataVed = 'a[data-ved]';
   const textFromLinkRelateds = 'Pesquisas relacionadas'
+  const teste = page.$
 
-  await page.evaluate((selectorAllLinksWithDataVed, textFromLinkRelateds) => {
+    await page.waitForSelector(selectorAllLinksWithDataVed, {timeout: 5000})
+    .catch(err => {
+      console.log('JAMIIIIIIIIIIIIIIKL')
+      return []
+    })
+
+  const isClicked = await page.evaluate((selectorAllLinksWithDataVed, textFromLinkRelateds) => {
     const allLinksWithDataVed = document.querySelectorAll(selectorAllLinksWithDataVed);
     const linkFromRelatedFiltered = Array.from(allLinksWithDataVed).filter(el => el.innerText === textFromLinkRelateds);
 
     if(linkFromRelatedFiltered.length) {
       const linkFromRelated = linkFromRelatedFiltered[0];
       linkFromRelated.click();
+      return true
     }
-  }, selectorAllLinksWithDataVed, textFromLinkRelateds)
 
-  await page.waitForNavigation();
+    return false
+  }, selectorAllLinksWithDataVed, textFromLinkRelateds)
+  
+  if(isClicked) {
+    await page.waitForNavigation();
+  }
 
   const selectorContainerRelateds = '#extabar';
   const selectorRelateds = 'a[data-entityname]';
@@ -37,6 +49,24 @@ async function collectData(page) {
 
 }
 
+async function collectData(page, initialMainName) {
+  let BDrelateds = {}
+  const relateds = await getRelateds(page);
+  BDrelateds[initialMainName] = relateds;
+  for (let x=0; x <= 10; x++) {
+    const queryForSearch = relateds[x].replace(/ /g,'+');
+    const url = getUrlForCrawling('search', queryForSearch);
+    await page.goto(url);
+    const relatedData = await getRelateds(page);
+    BDrelateds[relateds[x]] = relatedData;
+  }
+
+  console.log(BDrelateds)
+  return {
+    relateds
+  }
+}
+
 function getUrlForCrawling(type, query) {
   const urls = {
     search: `https://www.google.com/search?q=${query}`
@@ -46,13 +76,15 @@ function getUrlForCrawling(type, query) {
 }
 
 async function startCrawling() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   console.log('Service of crawling started...');
   const page = await browser.newPage();
-  const url = getUrlForCrawling('search', 'Hot+Park');
+  const initialMainName = 'Hot Park';
+  const queryForSearch = initialMainName.replace(/ /g,'+');
+  const url = getUrlForCrawling('search', queryForSearch);
   await page.goto(url);
 
-  const collectedData = await collectData(page);
+  const collectedData = await collectData(page, initialMainName);
 
   console.log(JSON.stringify(collectedData, null, 2));
   
